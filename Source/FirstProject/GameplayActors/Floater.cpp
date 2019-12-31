3b8f9d83111f4164495dcd0ab13924d7916a53ea
Chris Whiting <mpro34@gmail.com>
2019-12-31 13:43:22 -0800

    Add sweeping instead of enabling physics

--- Source/FirstProject/GameplayActors/Floater.cpp
+++ Source/FirstProject/GameplayActors/Floater.cpp
@@ -44,7 +44,10 @@ void AFloater::Tick(float DeltaTime)
 	if (bShouldFloat)
 	{
 		FHitResult HitResult;
-		AddActorLocalOffset(InitialDirection, false, &HitResult);
+		AddActorLocalOffset(InitialDirection, true, &HitResult);
+
+		FVector HitLocation = HitResult.Location;
+
 	}
 }
 
